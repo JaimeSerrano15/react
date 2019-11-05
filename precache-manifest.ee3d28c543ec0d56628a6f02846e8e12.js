bd6a1b351bd12bddaffe9fc22d39685eb7bdc628
JaimeSerrano15 <00025318@uca.edu.sv>
2019-11-04 23:29:24 -0600

    Updates

---
+++ precache-manifest.ee3d28c543ec0d56628a6f02846e8e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b010cd6f894a269f04ca88efd3954289",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "082ea250ba7c04dc58d4",
+    "url": "/react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ec46778c945423c048eb",
+    "url": "/react/static/js/2.4df0e7f9.chunk.js"
+  },
+  {
+    "revision": "082ea250ba7c04dc58d4",
+    "url": "/react/static/js/main.ae8bd26d.chunk.js"
+  },
+  {
+    "revision": "9230ed9b3262159a2c91",
+    "url": "/react/static/js/runtime-main.9b0c0744.js"
+  }
+]);
